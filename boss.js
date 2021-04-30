2f1532025458846a7559a6fe56fd32d5000e76b5
fruttis1337 <fruttis1337@mail.ru>
2021-04-30 22:23:19 +1000

    add gitignore

---
+++ boss.js
@@ -0,0 +1,19 @@
+class Boss{
+    constructor(x, y, dx, dy) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    draw() {
+        var enemy = new Image();
+        enemy.src = 'Images/boss.png';
+        context.drawImage(enemy, this.x, this.y, 200, 200);
+    }
+
+    move() {
+        this.x = this.x + this.dx;
+        this.y = this.y + this.dy;
+    }
+}
