5ec79dbed8c1a3c5d0d31f4aa8c14f4abe16948a
fruttis1337 <fruttis1337@mail.ru>
2021-04-30 22:20:15 +1000

    create enemy class

--- game.js
+++ game.js
@@ -1,9 +1,35 @@
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
 
+
 var background = new Image();
 background.src = 'Images/forest.png';
 
-background.onload = function(){
-    context.drawImage(background, 0, 0, 600, 600)
+
+background.onload = function () {
+    game();
 }
+
+function game() {
+    update();
+    render();
+    requestAnimationFrame(game);
+}
+
+function update() {
+}
+
+function render() {
+    context.drawImage(background, 0, 0, 700, 400);
+}
+
+var requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 20);
+        };
+})();
